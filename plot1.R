556ad2ed516742bc5ec22b299293d7d21639bed8
henryyip <wind_feynman@yahoo.com>
2014-07-12 16:12:59 +0800

    Added comments

--- plot1.R
+++ plot1.R
@@ -2,6 +2,7 @@ initial <- read.table("household_power_consumption.txt", header = TRUE, sep = ";
 classes <- sapply(initial, class)
 df <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?", stringsAsFactors=FALSE, colClasses = classes, comment.char="")
 
+# Take subset with the two specified dates: 1/2/2007 and 2/2/2007
 df2day <- subset(df, Date == "1/2/2007" | Date == "2/2/2007")
 
 hist(df2day$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
